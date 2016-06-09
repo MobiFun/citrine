3638f239ea5d6ac510a9cc71480e603411dd12b8
Mychaela Falconia <falcon@freecalypso.org>
2016-06-09 00:53:13 +0000

    L1/include/l1_varex.h: same data section fix as in the LoCosto-based version

--- L1/include/l1_varex.h
+++ L1/include/l1_varex.h
@@ -9,28 +9,20 @@
 
 #ifdef L1_ASYNC_C
 
-#if (LONG_JUMP == 3)
- #pragma DATA_SECTION(l1s,".l1s_global")
- #pragma DATA_SECTION(l1s_dsp_com,".l1s_global")
- #pragma DATA_SECTION(l1a_l1s_com,".l1s_global")
- #pragma DATA_SECTION(l1s_tpu_com,".l1s_global")
- #pragma DATA_SECTION(l1_config,".l1s_global")
-#endif
-
- T_L1S_GLOBAL   l1s;
+ T_L1S_GLOBAL   l1s __attribute__ ((section (".l1s_global")));
  T_L1A_GLOBAL   l1a;
 
- T_L1A_L1S_COM  l1a_l1s_com;
- T_L1S_DSP_COM  l1s_dsp_com;
- T_L1S_TPU_COM  l1s_tpu_com;
+ T_L1A_L1S_COM  l1a_l1s_com __attribute__ ((section (".l1s_global")));
+ T_L1S_DSP_COM  l1s_dsp_com __attribute__ ((section (".l1s_global")));
+ T_L1S_TPU_COM  l1s_tpu_com __attribute__ ((section (".l1s_global")));
 
  #if (L1_DYN_DSP_DWNLD == 1)    // equivalent to an API_HISR flag
- T_L1_API_HISR       l1_apihisr;
- T_L1A_API_HISR_COM l1a_apihisr_com;
-#endif
+  T_L1_API_HISR      l1_apihisr __attribute__ ((section (".l1s_global")));
+  T_L1A_API_HISR_COM l1a_apihisr_com __attribute__ ((section (".l1s_global")));
+ #endif
 
  // variables for L1 configuration
- T_L1_CONFIG    l1_config;
+ T_L1_CONFIG    l1_config __attribute__ ((section (".l1s_global")));
 
 #else  // L1_ASYNC_C
 
@@ -42,9 +34,9 @@
  extern T_L1S_TPU_COM  l1s_tpu_com;
 
  #if (L1_DYN_DSP_DWNLD == 1)    // equivalent to an API_HISR flag
- extern T_L1_API_HISR      l1_apihisr;
- extern T_L1A_API_HISR_COM l1a_apihisr_com;
-#endif
+  extern T_L1_API_HISR      l1_apihisr;
+  extern T_L1A_API_HISR_COM l1a_apihisr_com;
+ #endif
 
  // variables for L1 configuration
  extern T_L1_CONFIG    l1_config;
